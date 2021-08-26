84a594caccfae3cfa19ac37a017760620524d2ba
Levente Bognar <lajos93@users.noreply.github.com>
2021-08-26 10:54:47 +0200

    Update article.js

--- models/article.js
+++ models/article.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const article = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  image: { type: String, required: true },
+  image: { type: String, required: false },
   body: { type: String, required: true },
   created: Number,
   updated: Number,
